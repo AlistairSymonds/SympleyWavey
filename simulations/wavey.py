d9f3f8332070fce22643392d2f4089e36bd0a4f7
Alistair Symonds <alistair.symonds@gmail.com>
2023-01-24 15:51:17 +1100

    pixel size updates for sensor lib

--- simulations/wavey.py
+++ simulations/wavey.py
@@ -4,16 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 class camera(detector.Detector):
 
-    def __init__(self, dark_current, read_noise, bias, fwc, conversion_gain, bits, exposure_time, sensor_width, sensor_height, sensor_width_npx, sensor_height_npx):
+    def __init__(self, dark_current, read_noise, bias, fwc, conversion_gain, bits, exposure_time, pixel_width, pixel_height, sensor_width_npx, sensor_height_npx):
         super().__init__(dark_current, read_noise, bias, fwc, conversion_gain, bits, exposure_time)
-        self.sensor_width = sensor_width
-        self.sensor_height = sensor_height
+
         self.sensor_width_npx = sensor_width_npx
         self.sensor_height_npx = sensor_height_npx
+        self.sensor_width = pixel_width * sensor_width_npx
+        self.sensor_height = pixel_height * sensor_height_npx
 
     def expose2(self, sim_image, sim_x, sim_y, peak_eminus):
     
-        
+        #warning warning warning to future me: simulation plane needs to be larger than sensor plane
 
         #now we need to take the area the the sensor will observe out of the simulation coordinate space and convert to pixels
         sensor_area_x = (self.sensor_width/2  >= sim_x) & (-1*self.sensor_width/2  <= sim_x)
@@ -48,8 +49,8 @@ class imx533(camera):
         conversion_gain=1, 
         bits=14, 
         exposure_time=14, 
-        sensor_width=11.31, 
-        sensor_height=11.31, 
+        pixel_width=3.76/1000.0, 
+        pixel_height=3.76/1000.0,
         sensor_width_npx=3008, 
         sensor_height_npx=3008)
 
@@ -62,7 +63,7 @@ class imx174(camera):
         conversion_gain=1, 
         bits=12,
         exposure_time=14, 
-        sensor_width=11.3, 
-        sensor_height=7.1, 
+        pixel_width=5.86/1000.0, 
+        pixel_height=5.86/1000.0, 
         sensor_width_npx=1936, 
         sensor_height_npx=1216)
