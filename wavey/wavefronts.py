6c80f1ce2bb2a636f5b5e5be6137c26dd47888b8
Alistair Symonds <alistair.symonds@gmail.com>
2023-01-30 18:27:42 +1100

    better results showing code

--- wavey/wavefronts.py
+++ wavey/wavefronts.py
@@ -1,6 +1,6 @@
 import numpy as np
 from prysm.polynomials import zernike_nm, zernike_nm_der_sequence, lstsq, nm_to_name, fringe_to_nm, zernike_nm_der
-from prysm import geometry
+from prysm import geometry, coordinates
 import matplotlib.pyplot as plt
 
 class WavefrontAnalysis:
@@ -12,21 +12,24 @@ class WavefrontAnalysis:
 
     def gen_wf(self, r, t):
         Z = np.zeros_like(r)
-        for nm in (self.z_wv):
-            Z += (self.z_wv[nm] * zernike_nm(nm[0], nm[1], r, t))
+        for j in (self.z_wv):
+            n, m = fringe_to_nm(j)
+            Z += (self.z_wv[j] * zernike_nm(n, m, r, t))
+        return Z
 
     def gen_der_wf(self, r, t):
         dZdr = np.zeros_like(r)
         dZdt = np.zeros_like(r)
-        for nm in (self.z_wv):
-            (dr, dt) = (zernike_nm_der(nm[0], nm[1], r, t))
-            dZdr += (self.z_wv[nm] * dr)
-            dZdt += (self.z_wv[nm] * dt)
+        for j in (self.z_wv):
+            n, m = fringe_to_nm(j)
+            (dr, dt) = (zernike_nm_der(n, m, r, t))
+            dZdr += (self.z_wv[j] * dr)
+            dZdt += (self.z_wv[j] * dt)
 
         return (dZdr, dZdt)
 
 def plot_zernike(z, r, t):
-     
+
     Z_img = z.copy()
     zernike_mask = geometry.circle(1, r)
     Z_img[zernike_mask!=1]=np.nan
@@ -34,8 +37,8 @@ def plot_zernike(z, r, t):
 
     fig = plt.figure()  
     ax = fig.add_subplot(1, 1, 1)
-    ax.set_title("Reconstructed wavefront error (in waves)")
-    ax.imshow(Z_img)
+    ax.set_title("Wavefront deviation (in waves)")
+    ax.imshow(Z_img, cmap='Spectral')
     plt.show()
 
 def plot_zernike_der(dr, dt, r, t):
