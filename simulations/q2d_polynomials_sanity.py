6cc4421405f70d042d3ceaf4f055b4a8236fe22a
AlistairSymonds <alistair.symonds@gmail.com>
2023-02-13 21:43:51 +1100

    add q2d poly test code

---
+++ simulations/q2d_polynomials_sanity.py
@@ -0,0 +1,85 @@
+import numpy as np
+from prysm.polynomials.qpoly import compute_z_zprime_Q2d, Q2d_nm_c_to_a_b
+from prysm.polynomials import fringe_to_nm, nm_to_name
+from prysm import geometry, coordinates
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from pathlib import Path
+
+def plot_q2d(fringes, coeffs):
+    
+    x, y = coordinates.make_xy_grid(2048, diameter=2)
+    r, t = coordinates.cart_to_polar(x, y)
+
+    z = np.zeros((2048, 2048))
+    dr = np.zeros((2048, 2048))
+    dt = np.zeros((2048, 2048))
+    
+    nms = []
+    for J, C in zip(fringes, coeffs):
+        (n, m) = fringe_to_nm(J)
+        print(f"(n: {n}, m: {m}) - {C}")
+        nms.append((n, m))
+
+
+    (cms, ams, bms )= Q2d_nm_c_to_a_b(nms, coeffs)
+    (_z, _dr, _dt) = compute_z_zprime_Q2d(cms, ams, bms, r, t)
+
+    z  += (_z)
+    dr += (_dr)
+    dt += (_dt)
+
+    # Mask out a circle for plotting
+    mask = geometry.circle(1, r)
+
+    z[mask!=1]=np.nan
+    dr[mask!=1]=np.nan
+    dt[mask!=1]=np.nan
+
+    #  Plotting
+    width = 20
+    fig = plt.figure(figsize=(width,width/3))
+
+    color_norm=colors.TwoSlopeNorm(vcenter=0.0)
+    z_ax = fig.add_subplot(1, 3, 1)
+    z_ax.imshow(z, norm=color_norm, cmap="RdBu", extent=[-1, 1, -1, 1])
+    z_ax.set_title("Phase")
+    fig.colorbar(z_ax.get_images()[0])
+    #plt.axis('off')
+
+
+    color_norm=colors.TwoSlopeNorm(vcenter=0.0)
+    dr_ax = fig.add_subplot(1, 3, 2)
+    dr_ax.imshow(dr, norm=color_norm, cmap="RdBu", extent=[-1, 1, -1, 1])
+    dr_ax.set_title("Radial derivative of phase")
+    fig.colorbar(dr_ax.get_images()[0])
+    #plt.axis('off')
+
+
+    color_norm=colors.TwoSlopeNorm(vcenter=0.0)
+    dt_ax = fig.add_subplot(1, 3, 3)
+
+    dt_ax.imshow(dt, norm=color_norm, cmap="RdBu", extent=[-1, 1, -1, 1])
+    dt_ax.set_title("Tangential derivative of phase")
+    fig.colorbar(dt_ax.get_images()[0])
+
+    #plt.axis('off')
+    #plt.tight_layout()
+    fig.suptitle("Q2D stuff")
+    #plt.savefig(file_path)
+    plt.show()
+    plt.close(fig)
+
+
+
+
+zernikes = np.arange(1, 9)
+coeffs = np.zeros(zernikes.shape)
+#coeffs[5] = 1
+#coeffs[4] = 0.5
+coeffs[7] = 10
+
+
+plot_q2d(zernikes, coeffs)
+        
+        
